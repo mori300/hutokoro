6cec76196d45954f6dcc3453e74719a03f6c6cc6
mori300 <mmori1435@gmail.com>
2021-03-04 16:22:30 +0900

    残高リセット機能の追加 BalanceResetBtn.vue

---
+++ src/components/Balance/BalanceResetBtn.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  #balance-reset-btn
+    .reset-btn(v-if="this.totalBalance.totalBalance != 0")
+      button(@click="balanceResetBtn()") 残高リセット
+</template>
+
+<script>
+import firebase from '/firebase/firestore.js'
+const db = firebase.firestore()
+const balanceRef = db.collection("Balance").doc("balance")
+export default {
+  data() {
+    return {
+      totalBalance: {}
+    }
+  },
+  created() {
+    balanceRef.onSnapshot(doc => {
+      this.totalBalance = doc.data()
+    })
+  },
+  methods: {
+    balanceResetBtn() {
+      balanceRef.update({
+        totalBalance: 0
+      })
+      .then(balanceRef => {
+        alert("残高をリセットしました")
+      })
+      .catch(error => {
+        alert("リセットができませんでした")
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  #balance-reset-btn {
+    
+    button {
+      height: 30px;
+      border: 1px solid black;
+      border-radius: 5px;
+      background-color: white;
+    }
+  }
+</style>
--- src/components/Balance/TotalBalance.vue
+++ src/components/Balance/TotalBalance.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
   #total-balance
     h1 ¥{{ this.totalBalance.totalBalance }}
+    balanceResetBtn
 </template>
 
 <script>
 import firebase from '/firebase/firestore.js'
+import balanceResetBtn from './BalanceResetBtn.vue'
 
 const db = firebase.firestore()
 const balanceRef = db.collection("Balance").doc("balance")
 export default {
+  components: {
+    balanceResetBtn
+  },
   data() {
     return {
       totalBalance: {}
@@ -21,3 +26,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  #total-balance {
+    margin-bottom: 30px;
+  }
+</style>
