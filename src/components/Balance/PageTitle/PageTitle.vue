8aa99fc6fd278b9aea4636b40d89c1d3b4f42dae
mori300 <mmori1435@gmail.com>
2021-03-11 00:40:04 +0900

    onAuthStateChangedでログイン中のユーザーemailを取得する

--- src/components/Balance/PageTitle/PageTitle.vue
+++ src/components/Balance/PageTitle/PageTitle.vue
@@ -1,16 +1,26 @@
 <template lang="pug">
   #page-title
-    h2 {{ name }}の残高
+    h2 {{ this.currentUser.email }}の残高
 </template>
 
 <script>
 import firebase from '/firebase/firestore.js'
 
+const db = firebase.firestore()
 export default {
   data() {
     return {
-      name: firebase.auth().currentUser.email
+      currentUser: {}
     }
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if(user) {
+        db.collection("users").doc(user.uid).onSnapshot(doc => {
+          this.currentUser = doc.data()
+        })
+      }
+    })
   }
 }
 </script>
